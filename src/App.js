4935942d5ad67ad6039a60fe46520882085f7b6a
yigithankardas <y.kardas@etu.edu.tr>
2022-10-25 18:21:04 +0300

    changed router logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router';
 import LeftNavBar from './components/LeftNavBar';
 import { Grid } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import Hello from './pages/Hello';
+import BlankPage from './pages/BlankPage';
 
 function App() {
   return (
-    <Grid container>
-      <LeftNavBar></LeftNavBar>
-      <Outlet />
-    </Grid>
+    <>
+      <Grid container>
+        <LeftNavBar />
+        <Routes>
+          <Route path="/hello" element={<Hello />} />
+          <Route path="/blank" element={<BlankPage />} />
+        </Routes>
+      </Grid>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,30 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'typeface-roboto';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import BlankPage from './pages/BlankPage';
-import Hello from './pages/Hello';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/hello',
-        element: <Hello />,
-      },
-      {
-        path: '/blank',
-        element: <BlankPage />,
-      },
-    ],
-  },
-]);
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
