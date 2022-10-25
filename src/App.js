c5d2d80268ccca81f9d28d7016c347dd4005e467
yigithankardas <y.kardas@etu.edu.tr>
2022-10-25 21:41:50 +0300

    trying auth

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import { Grid } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Hello from './pages/Hello';
 import BlankPage from './pages/BlankPage';
+import LoginPage from './pages/LoginPage';
+import { RequireAuth } from 'react-auth-kit';
 
 function App() {
   return (
-    <>
-      <Grid container>
-        <LeftNavBar />
-        <Routes>
-          <Route path="/hello" element={<Hello />} />
-          <Route path="/blank" element={<BlankPage />} />
-        </Routes>
-      </Grid>
-    </>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth loginPath={'/login'}>
+            <Grid container>
+              <LeftNavBar />
+            </Grid>
+          </RequireAuth>
+        }
+      />
+      <Route path="/hello" element={<Hello />} />
+      <Route path="/blank" element={<BlankPage />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'typeface-roboto';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider
+      authType={'cookie'}
+      authName={'_auth'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
